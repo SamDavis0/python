b5a25b6367b8bf45b2dbb7e89ae7aa1cd28b0042
Sam Davis <hsndm16@yahoo.com>
2021-09-08 19:19:52 -0500

    finished homework

--- homework/python101.py
+++ homework/python101.py
@@ -70,5 +70,9 @@
 #     stars += 2
 
 
-for x in range(1, 11):
-    print(2 * x)
+
+#MULTIPLICATION TABLE
+
+# for x in range(1, 11):
+#     for y in range(1, 11):
+#         print(f"{x} x {y} = {x * y}")
